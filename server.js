eaefa00e25f08659ba11424c5264a6749b7fd51e
idmjungle <idmjungle@gmail.com>
2023-02-20 08:36:13 -0600

    Next Update

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+    res.status(200).json({ success:true, data: { id: 1 } });
+});
+
 app.listen(
     PORT, 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
