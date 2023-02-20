c84142da407b95c9edb31358a377427e1e81bb5d
idmjungle <idmjungle@gmail.com>
2023-02-20 08:36:43 -0600

    Create Routes

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
 
 // Load env var
-
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// dDev logging middleware
 
-app.get('/', (req, res) => {
-    res.status(200).json({ success:true, data: { id: 1 } });
-});
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
     PORT, 
